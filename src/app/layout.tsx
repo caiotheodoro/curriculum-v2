03334586f427495e259ac3d93d984b4cc429ad2d
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-04-10 21:48:48 -0300

    fix: 🐛 analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import { LayoutProps } from "@/@types/common";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export default function RootLayout({ children }: Readonly<LayoutProps>) {
   return (<>
   <Analytics/>
+  <SpeedInsights/>
   {children}
   </>);
 }
