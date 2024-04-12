a88dc3700b9407b1591360b44ffa8944c6a1f72c
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-04-12 17:38:31 -0300

    fix: 🐛 LCP time reduced

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,11 @@
-import Hydrate from "@/utils/query/hydrate-client";
-import { dehydrate } from "@tanstack/react-query";
 import { useTranslations } from "next-intl";
-import { createSSRHelper } from "@/app/api/trpc/trpc-router";
 import { FadeIn } from "@/app/components/atoms/fade-in";
 import { ShakeAnimation } from "@/app/components/atoms/squeeze";
-import { MarkdownRenderer } from "@/app/components/molecules/journey/markdown-renderer";
 import { CodeBlock } from "@/app/components/organisms/code-block/code-block";
 import { getExperience } from "@/utils/format";
 import { CarouselStack } from "@/app/components/organisms/card-carousel/card-carousel";
 export default function Home() {
   const t = useTranslations("about");
-  const helpers = createSSRHelper();
 
   const formatObject = () => {
     return `{
@@ -26,24 +21,20 @@ export default function Home() {
   };
 
   return (
-    <Hydrate state={dehydrate(helpers.queryClient)}>
-      <FadeIn className="flex lg:gap-12 gap-10 flex-col text-justify">
-        <div className="flex gap-2 lg:text-base md:text-sm sm:text-sm text-sm ">
-          <ShakeAnimation>👋</ShakeAnimation>
-          {t("title")}
-        </div>
-        <MarkdownRenderer>
-          {t.markup("description", {
-            span: (chunks) =>
-              `<span className="lg:text-base md:text-sm sm:text-sm text-sm ">${chunks}</span>`,
-          })}
-        </MarkdownRenderer>
-        <div>
-          <CodeBlock title={t("personal.title")} code={formatObject()} />
-        </div>
-        {t("certificates")}
-        <CarouselStack />
-      </FadeIn>
-    </Hydrate>
+    <FadeIn className="flex lg:gap-12 gap-10 flex-col text-justify">
+      <div className="flex gap-2 lg:text-base md:text-sm sm:text-sm text-sm ">
+        <ShakeAnimation>👋</ShakeAnimation>
+        {t("title")}
+      </div>
+      <div className="flex gap-3 flex-col">
+        <span>{t("description")}</span>
+        <span>{t("subdescription")}</span>
+      </div>
+      <div>
+        <CodeBlock title={t("personal.title")} code={formatObject()} />
+      </div>
+      {t("certificates")}
+      <CarouselStack />
+    </FadeIn>
   );
 }
